9c958a45097ed6f101a6907dd2475f857a5bdb45
Mike North <michael.l.north@gmail.com>
2018-09-17 17:20:44 -0700

    TS conversion - remove generated JS files

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class SparklesComponent<T = object> {
-    args: T;
-    constructor(args: T);
-    didInsertElement(): void;
-    didUpdate(): void;
-    destroy(): void;
-}
-export default SparklesComponent;
-export { tracked } from './tracked';
--- tracked.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function tracked(target: object, propertyKey: string | symbol, descriptor?: PropertyDescriptor): any;
-export declare function tracked(...args: string[]): any;
