55bc7f9a163c0b8805b3349f83fbcd2800f76c61
Mike North <michael.l.north@gmail.com>
2018-11-28 23:20:31 +0800

    chore: tests

---
+++ node-tests/blueprints/sparkle-test.js
@@ -0,0 +1,156 @@
+'use strict';
+
+const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
+const emberNew = blueprintHelpers.emberNew;
+const { emberGenerateDestroy } = blueprintHelpers;
+
+const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
+
+describe('Classic App Layout: ember generate and destroy a sparkle component', function() {
+  setupTestHooks(this);
+
+  it('ember g sparkle x-foo', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew().then(() =>
+      emberGenerateDestroy(['sparkle', 'x-foo'], file => {
+        expect(file('app/components/x-foo.js')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XFoo extends Component {
+
+}
+`
+        );
+        expect(file('app/templates/components/x-foo.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+      })
+    );
+  });
+  it('ember g sparkle x-foo --lang ts', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew().then(() =>
+      emberGenerateDestroy(['sparkle', 'x-foo', '--lang', 'ts'], file => {
+        expect(file('app/components/x-foo.ts')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XFoo extends Component {
+
+}
+`
+        );
+        expect(file('app/templates/components/x-foo.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+      })
+    );
+  });
+  it('ember g sparkle x-foo --lang js', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew().then(() =>
+      emberGenerateDestroy(['sparkle', 'x-foo', '--lang', 'js'], file => {
+        expect(file('app/components/x-foo.js')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XFoo extends Component {
+
+}
+`
+        );
+        expect(file('app/templates/components/x-foo.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+      })
+    );
+  });
+});
+
+describe('Classic Addon Layout: ember generate and destroy a sparkle component', function() {
+  setupTestHooks(this);
+
+  it('ember g sparkle x-boz', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew({ target: 'addon' }).then(() =>
+      emberGenerateDestroy(['sparkle', 'x-boz'], file => {
+        expect(file('addon/components/x-boz.js')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XBoz extends Component {
+
+}
+`
+        );
+        expect(file('addon/templates/components/x-boz.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+        expect(file('app/components/x-boz.js')).to.eq(
+          `export { default } from 'my-addon/components/x-boz';
+`
+        );
+        expect(file('app/templates/components/x-boz.js')).to.eq(
+          `export { default } from 'my-addon/templates/components/x-boz';
+`
+        );
+      })
+    );
+  });
+  it('ember g sparkle x-baz --lang js', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew({ target: 'addon' }).then(() =>
+      emberGenerateDestroy(['sparkle', 'x-baz', '--lang', 'js'], file => {
+        expect(file('addon/components/x-baz.js')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XBaz extends Component {
+
+}
+`
+        );
+        expect(file('addon/templates/components/x-baz.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+        expect(file('app/components/x-baz.js')).to.eq(
+          `export { default } from 'my-addon/components/x-baz';
+`
+        );
+        expect(file('app/templates/components/x-baz.js')).to.eq(
+          `export { default } from 'my-addon/templates/components/x-baz';
+`
+        );
+      })
+    );
+  });
+  it('ember g sparkle x-biz --lang ts', function() {
+    // pass any additional command line options in the arguments array
+    return emberNew({ target: 'addon' }).then(() =>
+      emberGenerateDestroy(['sparkle', 'x-biz', '--lang', 'ts'], file => {
+        expect(file('addon/components/x-biz.ts')).to.eq(
+          `import Component, { tracked } from 'sparkles-component';
+
+export default class XBiz extends Component {
+
+}
+`
+        );
+        expect(file('addon/templates/components/x-biz.hbs')).to.eq(
+          `{{yield}}
+`
+        );
+        expect(file('app/components/x-biz.js')).to.eq(
+          `export { default } from 'my-addon/components/x-biz';
+`
+        );
+        expect(file('app/templates/components/x-biz.js')).to.eq(
+          `export { default } from 'my-addon/templates/components/x-biz';
+`
+        );
+      })
+    );
+  });
+});
