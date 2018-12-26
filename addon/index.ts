36121d6f91f11cf43dba95203d2448d5cb7fc1b7
gossi <gossi@users.noreply.github.com>
2018-12-26 17:22:46 +0100

    fix component deprecation warning

--- addon/index.ts
+++ addon/index.ts
@@ -1,4 +1,6 @@
+import ApplicationInstance from '@ember/application/instance';
 import { setComponentManager } from '@ember/component';
+import SparklesComponentManager from './component-managers/sparkles';
 
 class SparklesComponent<T = object> {
   constructor(public args: T) {}
@@ -9,8 +11,12 @@ class SparklesComponent<T = object> {
   // didRender() {}
   destroy() {}
 }
-setComponentManager('sparkles', SparklesComponent);
+
+setComponentManager((owner: ApplicationInstance) => {
+  return new SparklesComponentManager(owner)
+}, SparklesComponent);
 
 export default SparklesComponent;
 
 export { tracked } from './tracked';
+
