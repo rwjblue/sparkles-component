158873bbe7e85dd08cb3aa2388a918a33ab5e157
Jan Buschtöns <jan@buschtoens.me>
2019-01-10 11:14:13 +0100

    chore(types): add types for new `setComponentManager`

--- types/@ember/component.d.ts
+++ types/@ember/component.d.ts
@@ -1,7 +1,14 @@
 declare module '@ember/component' {
   export function setComponentManager<T>(managerId: string, baseClass: T): T;
-  export function capabilities(version: string, opts?: {
+  export function setComponentManager<T>(
+    managerFactory: (owner: Owner) => {},
+    baseClass: T
+  ): T;
+  export function capabilities(
+    version: string,
+    opts?: {
       destructor?: boolean;
       asyncLifecycleCallbacks?: boolean;
-    }): any;
+    }
+  ): any;
 }
